cbd4ae16c448d22a8d9b6c6964a5587148dcabed
chenxbook <chenxbook@126.com>
2019-12-14 11:22:26 +0800

    python中函数的定义与调用

---
+++ sample/python_function.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+# 定义函数的一般语法：
+
+
+def greeting():
+    """显示简单的问候语"""
+    print("Hello World!")
+
+
+def printme(strings):
+    """打印任何参入的字符串"""
+    print(strings)
+    return
+
+
+# 1.函数的调用
+greeting()
+printme("我要调用用户自定义函数")
+printme("再次调用同一函数")
+
